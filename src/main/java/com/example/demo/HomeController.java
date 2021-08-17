be7b31d07463acf47f9bb237813424249550147a
rey-gbtc <reynaldo.apontedavila@montgomerycollege.edu>
2021-08-17 09:10:52 -0400

    line 10 to 8

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;   // this line was after stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;   // this line was after stereotype.Controller
 
 import javax.validation.Valid;
 import java.security.Principal;
